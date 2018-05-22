9ad35126a23c5f7099d4671ef22d320e510aa6cf
Jade Meskill <jade.meskill@gmail.com>
2018-05-21 17:18:59 -0700

    add yarn to init

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -3,11 +3,26 @@
 brew tap thoughtbot/formulae
 brew install rcm
 brew install \
+  direnv \
   fish \
-  direnv
+  hub \
+  rbenv \
+  yarn 
 
 rcup -v
 
-mkdir -p ~/.bin ~/go/bin ~/config/yarn/global
+mkdir -p \
+  ~/.bin \
+  ~/go/bin \
+  ~/config/yarn/global
+
+yarn config set prefix ~/.config/yarn/global
+
+yarn global add \
+  eslint \
+  hs \
+  wscat \
+  prettier
+
 
 curl -L https://get.oh-my.fish | fish
