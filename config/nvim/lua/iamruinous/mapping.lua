a1d21789329f85deed3c805ff999594c1bcbb961
Jade Meskill <jade.meskill@gmail.com>
2021-12-15 23:23:35 -0700

    fix screen-256color for tmux, add telescope key bindings

--- config/nvim/lua/iamruinous/mapping.lua
+++ config/nvim/lua/iamruinous/mapping.lua
@@ -6,3 +6,9 @@ m.nnoremap("<leader>xd", ":Trouble lsp_workspace_diagnostics<cr>", "Trouble: LSP
 m.nnoremap("<leader>xl", ":Trouble loclist<cr>", "Trouble: loclist", "silent")
 m.nnoremap("<leader>xq", ":Trouble loclist<cr>", "Trouble: Quickfix", "silent")
 m.nnoremap("gR", ":Trouble lsp_references<cr>", "Trouble: LSP References", "silent")
+
+-- telescope keybindings
+m.nnoremap("<leader>ff", ":Telescope find_files<cr>", "Telescope: files")
+m.nnoremap("<leader>fg", ":Telescope live_grep<cr>", "Telescope: grep")
+m.nnoremap("<leader>fb", ":Telescope buffers<cr>", "Telescope: buffers")
+m.nnoremap("<leader>fh", ":Telescope help_tags<cr>", "Telescope: tags")
--- config/tmux/common.tmux
+++ config/tmux/common.tmux
@@ -7,5 +7,10 @@ set-option -g bell-action none
 
 set -g base-index 1
 set-window-option -g pane-base-index 1
-#set -g default-terminal "xterm-256color"
+set -g default-terminal "screen-256color"
 #set-option -ga terminal-overrides ",*256col*:RGB"
+set -as terminal-features ",xterm-256color:RGB"
+
+bind R \
+  move-window -r\; \
+  display-message "Windows reordered..."
