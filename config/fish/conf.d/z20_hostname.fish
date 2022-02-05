d16a74cb64c6e753f1e275349146d4f7aee4eabd
Jade Meskill <jade.meskill@gmail.com>
2022-02-05 16:35:32 -0700

    use future for long names

--- config/fish/conf.d/z20_hostname.fish
+++ config/fish/conf.d/z20_hostname.fish
@@ -3,10 +3,10 @@ if status --is-interactive; and type -q toilet; and type -q lolcat
     set TOILETNAME (hostname -s)
   end
   if test -z "$TOILETFONT"
-    if test (string length "$TOILETNAME") -gt 10
-      set TOILETFONT "smblock"
+    if test (string length "$TOILETNAME") -gt 15
+      set TOILETFONT "future"
     else
-      set TOILETFONT "smmono12"
+      set TOILETFONT "smblock"
     end
   end
   echo "$TOILETNAME" | toilet -f "$TOILETFONT" | lolcat
