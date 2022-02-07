191e653415200a32a6bdebc6a2fb81d37708c25f
Jade Meskill <jade.meskill@gmail.com>
2022-02-07 00:58:14 +0000

    allow settable maxlength for hostname font

--- config/fish/conf.d/z20_hostname.fish
+++ config/fish/conf.d/z20_hostname.fish
@@ -1,9 +1,12 @@
 if status --is-interactive; and type -q toilet; and type -q lolcat
+  if test -z "$TOILETMAXLENGTH"
+    set TOILETMAXLENGTH 16
+  end
   if test -z "$TOILETNAME"
     set TOILETNAME (hostname -s)
   end
   if test -z "$TOILETFONT"
-    if test (string length "$TOILETNAME") -gt 15
+    if test (string length "$TOILETNAME") -gt $TOILETMAXLENGTH
       set TOILETFONT "future"
     else
       set TOILETFONT "smblock"
