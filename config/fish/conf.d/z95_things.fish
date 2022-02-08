20a1035b53ad44bc3f7ca01067f6bc070630e3bd
Jade Meskill <jade.meskill@gmail.com>
2022-02-07 17:14:49 -0700

    add things reminder to login

---
+++ config/fish/conf.d/z95_things.fish
@@ -0,0 +1,16 @@
+if status --is-interactive; and type -q things-cli
+  set -l things_today (things-cli today)
+  set -l things_count (count $things_today)
+  echo ""
+  set_color green; echo -n "Today's Top Things [$things_count]"
+  set -l inbox_count (things-cli inbox | wc -l | string trim)
+  if test $inbox_count -gt 0
+    set_color grey; echo -n " | "
+    set_color yellow; echo "Inbox [$inbox_count]"
+    set_color normal
+  end
+  echo ""
+  set_color green; printf "%s\n" $things_today[1..5]
+  set_color normal
+  echo ""
+end
