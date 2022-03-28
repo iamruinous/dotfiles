2a1d7a3cda88530f99a85949abc2c11e34e07b4a
Jade Meskill <jade.meskill@gmail.com>
2022-03-28 08:01:59 -0700

    update prompt, use --inherit for keychain

--- config/fish/conf.d/keychain.fish
+++ config/fish/conf.d/keychain.fish
@@ -1,3 +1,3 @@
 if status --is-interactive; and type -q keychain
-  eval (keychain --eval --quiet --agents ssh,gpg id_ed25519 id_jademeskill_ed25519 00AB98E4EDFA6211 CA5500A8F3A920A0)
+  eval (keychain --eval --inherit any --quick --quiet --agents ssh,gpg id_ed25519 id_jademeskill_ed25519 00AB98E4EDFA6211 CA5500A8F3A920A0)
 end
