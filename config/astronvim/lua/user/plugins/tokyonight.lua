47285074e6280bef694e64325c5a8f1950389ece
Jade Meskill <jade.meskill@gmail.com>
2023-07-21 19:56:50 -0700

    update astrovim theme to night

--- config/astronvim/lua/user/plugins/tokyonight.lua
+++ config/astronvim/lua/user/plugins/tokyonight.lua
@@ -3,7 +3,7 @@ return {
   -- lazy = false,
   priority = 1000,
   opts = {
-    style = "storm",                     -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+    style = "night",                     -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
     transparent = vim.g.neovide == nil, -- Enable this to disable setting the background color
     terminal_colors = true,             -- Configure the colors used when opening a `:terminal` in Neovim                                                                                                  |
     styles = {
