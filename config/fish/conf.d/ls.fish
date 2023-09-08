06f781c0e3266d28c9047f6f4a1cdb59dc55b811
Jade Meskill <jade.meskill@gmail.com>
2023-09-07 19:58:26 -0700

    switch to eza since exa is unmaintained, add ssh symbol

--- config/fish/conf.d/ls.fish
+++ config/fish/conf.d/ls.fish
@@ -1,5 +1,5 @@
-if status --is-interactive; and type -q exa
-  alias ls="exa --git --icons"
+if status --is-interactive; and type -q eza
+  alias ls="eza --git --icons"
 end
 alias ll="ls -lahF"
 alias lt="ls --tree"
