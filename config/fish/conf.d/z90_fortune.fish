47cb3438e131f2786ae597e000ef30e4845ae706
Jade Meskill <jade.meskill@gmail.com>
2023-10-14 23:36:21 -0700

    check for hotkey window

--- config/fish/conf.d/z90_fortune.fish
+++ config/fish/conf.d/z90_fortune.fish
@@ -1,6 +1,8 @@
 if status --is-interactive; and type -q fortune
-  set_color brblack
-  echo ""
-  fortune
-  set_color normal; echo ""
+  if [ $ITERM_PROFILE != "Hotkey Window" ]
+    set_color brblack
+    echo ""
+    fortune
+    set_color normal; echo ""
+  end
 end
--- config/fish/conf.d/z96_task.fish
+++ config/fish/conf.d/z96_task.fish
@@ -1,8 +1,10 @@
 if status --is-interactive; and type -q task
-  echo ""
-  set_color blue; echo -n "Next Tasks"
-  echo ""
-  set_color blue; task next
-  set_color normal
-  echo ""
+  if [ $ITERM_PROFILE != "Hotkey Window" ]
+    echo ""
+    set_color blue; echo -n "Next Tasks"
+    echo ""
+    set_color blue; task next
+    set_color normal
+    echo ""
+  end
 end
