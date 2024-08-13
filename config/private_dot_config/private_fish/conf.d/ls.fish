d31162a98e18537a914df283f0309a3eed7411c5
Jade Meskill <jade.meskill@gmail.com>
2024-08-12 17:04:29 -0700

    remove fish and ssh config, add wezterm

--- config/private_dot_config/private_fish/conf.d/ls.fish
+++
@@ -1,5 +0,0 @@
-if status --is-interactive; and type -q eza
-  alias ls="eza --git --icons"
-end
-alias ll="ls -lahF"
-alias lt="ls --tree"
--- config/private_dot_config/private_fish/conf.d/z90_fortune.fish
+++
@@ -1,8 +0,0 @@
-if status --is-interactive; and type -q fortune
-  if [ "$ITERM_PROFILE" != "Hotkey Window" ]
-    set_color brblack
-    echo ""
-    fortune
-    set_color normal; echo ""
-  end
-end
