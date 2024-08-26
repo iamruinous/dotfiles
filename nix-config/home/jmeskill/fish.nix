abac9a1cdef2aceeda1e0c0f86b7ed54e388930d
Jade Meskill <jade.meskill@gmail.com>
2024-08-26 09:27:01 -0700

    add fish init

--- nix-config/home/jmeskill/fish.nix
+++ nix-config/home/jmeskill/fish.nix
@@ -98,5 +98,69 @@
         src = fzf-fish.src;
       }
     ];
+
+    interactiveShellInit = ''
+      if type -q toilet; and type -q lolcat
+        set -q TOILETMAXLENGTH || set TOILETMAXLENGTH 16
+        set -q TOILETNAME || set TOILETNAME (hostname -s)
+        if ! set -q TOILETFONT
+          if test (string length "$TOILETNAME") -gt $TOILETMAXLENGTH
+            set TOILETFONT "future"
+          else
+            set TOILETFONT "smblock"
+          end
+        end
+        echo "$TOILETNAME" | toilet -f "$TOILETFONT" | lolcat
+      end
+
+      if [ "$ITERM_PROFILE" != "Hotkey Window" ]
+        if type -q fortune
+          set_color brblack
+          echo ""
+          fortune
+          set_color normal; echo ""
+        end
+
+        if type -q dfrs
+          dfrs
+          echo ""
+        end
+      end
+
+      # TokyoNight Color Palette
+      set -l foreground c0caf5
+      set -l selection 283457
+      set -l comment 565f89
+      set -l red f7768e
+      set -l orange ff9e64
+      set -l yellow e0af68
+      set -l green 9ece6a
+      set -l purple 9d7cd8
+      set -l cyan 7dcfff
+      set -l pink bb9af7
+
+      # Syntax Highlighting Colors
+      set -g fish_color_normal $foreground
+      set -g fish_color_command $cyan
+      set -g fish_color_keyword $pink
+      set -g fish_color_quote $yellow
+      set -g fish_color_redirection $foreground
+      set -g fish_color_end $orange
+      set -g fish_color_error $red
+      set -g fish_color_param $purple
+      set -g fish_color_comment $comment
+      set -g fish_color_selection --background=$selection
+      set -g fish_color_search_match --background=$selection
+      set -g fish_color_operator $green
+      set -g fish_color_escape $pink
+      set -g fish_color_autosuggestion $comment
+
+      # Completion Pager Colors
+      set -g fish_pager_color_progress $comment
+      set -g fish_pager_color_prefix $cyan
+      set -g fish_pager_color_completion $foreground
+      set -g fish_pager_color_description $comment
+      set -g fish_pager_color_selected_background --background=$selection
+    '';
   };
 }
