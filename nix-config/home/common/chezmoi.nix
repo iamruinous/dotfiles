f857696ec6b901c58f40b13c03edf440c3a10e0e
Jade Meskill <jade.meskill@gmail.com>
2024-12-10 22:40:13 -0700

    remove verbose from chezmoi

--- nix-config/home/common/chezmoi.nix
+++ nix-config/home/common/chezmoi.nix
@@ -14,7 +14,7 @@ in
   home.activation.chezmoi = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     # echo -e "\033[0;34mActivating chezmoi"
     # echo -e "\033[0;34m=================="
-    ${pkgs.chezmoi}/bin/chezmoi apply --source=${homeDir}/Projects/github/iamruinous/dotfiles/config --verbose --no-tty --keep-going --refresh-externals=never
+    ${pkgs.chezmoi}/bin/chezmoi --source=${homeDir}/Projects/github/iamruinous/dotfiles/config apply --no-tty --keep-going --refresh-externals=never
     # echo -e "\033[0;34m=================="
   '';
 }
