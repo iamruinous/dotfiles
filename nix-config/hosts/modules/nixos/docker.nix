bab14e6dc98e59a5fa0c8d8a7379c71fccd5c658
Jade Meskill <jade.meskill@gmail.com>
2025-02-17 21:38:30 -0700

    add lazydocker, get sddm themes to work

--- nix-config/hosts/modules/nixos/docker.nix
+++ nix-config/hosts/modules/nixos/docker.nix
@@ -1,6 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   # Docker configuration
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless.enable = true;
   virtualisation.docker.rootless.setSocketVariable = true;
+
+  environment.systemPackages = with pkgs; [
+    lazydocker
+  ];
 }
--- nix-config/hosts/modules/nixos/kde.nix
+++ nix-config/hosts/modules/nixos/kde.nix
@@ -1,9 +1,22 @@
-{...}: {
+{pkgs, ...}: {
   # Input settings
   services.libinput.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.extraPackages = with pkgs; [
+    kdePackages.qtsvg
+    kdePackages.qtmultimedia
+    kdePackages.qtvirtualkeyboard
+  ];
+  services.displayManager.sddm.theme = "sddm-astronaut-theme";
   services.desktopManager.plasma6.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    (sddm-astronaut.override
+      {
+        embeddedTheme = "pixel_sakura";
+      })
+  ];
 }
