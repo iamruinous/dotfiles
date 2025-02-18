a81c525ffccfb975d2f076935b6504b964045ebe
Jade Meskill <jade.meskill@gmail.com>
2025-02-17 18:41:43 -0700

    add inspo home manager

---
+++ nix-config/home/jmeskill/inspo.nix
@@ -0,0 +1,14 @@
+{...}: {
+  imports = [
+    ../modules/common.nix
+  ];
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.11";
+}
