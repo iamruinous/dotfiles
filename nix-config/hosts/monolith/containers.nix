3d22e88a6f190a778d7353f1d0541a2180d20285
Jade Meskill <jade.meskill@gmail.com>
2025-02-21 19:43:19 -0700

    add more services to monolith

--- nix-config/hosts/monolith/containers.nix
+++ nix-config/hosts/monolith/containers.nix
@@ -16,6 +16,69 @@
         };
       };
       services = {
+        # hostnet services
+        "caddy".service = {
+          container_name = "caddy";
+          image = "ghcr.io/caddybuilds/caddy-cloudflare:latest";
+          networks = [
+            "hostnet"
+            "proxynet"
+          ];
+          ports = [
+            "80:80"
+            "443:443"
+            "443:443/udp"
+            "2019:2019"
+          ];
+          capabilities = {
+            NET_ADMIN = true;
+          };
+          healthcheck = {
+            test = [
+              "CMD"
+              "wget"
+              "--no-verbose"
+              "--tries=1"
+              "--spider"
+              "http://127.0.0.1:2019/metrics"
+            ];
+            start_period = "60s";
+            interval = "60s";
+            timeout = "5s";
+            retries = 3;
+          };
+          restart = "unless-stopped";
+          volumes = [
+            "${config.age.secrets.monolith_caddy_caddyfile.path}:/etc/caddy/Caddyfile"
+            "/data/docker/caddy/site:/srv"
+            "/data/docker/caddy/data:/data"
+            "/data/docker/caddy/config:/config"
+          ];
+        };
+        # datanet services
+        "mariadb".service = {
+          container_name = "mariadb";
+          image = "docker.io/mariadb:latest";
+          env_file = [config.age.secrets.monolith_docker_env_mariadb.path];
+          networks = ["datanet"];
+          healthcheck = {
+            test = [
+              "CMD"
+              "healthcheck.sh"
+              "--connect"
+              "--innodb_initialized"
+            ];
+            start_period = "10s";
+            interval = "60s";
+            timeout = "5s";
+            retries = 3;
+          };
+          restart = "unless-stopped";
+          volumes = [
+            "/data/docker/mariadb/mysql:/var/lib/mysql"
+          ];
+        };
+        # proxynet services
         "pinchflat".service = {
           container_name = "pinchflat";
           image = "ghcr.io/kieraneglin/pinchflat:latest";
@@ -29,39 +92,6 @@
             "/nas/media/YT:/downloads"
           ];
         };
-        "caddy".service = {
-          container_name = "caddy";
-          image = "ghcr.io/caddybuilds/caddy-cloudflare:latest";
-          networks = [
-            "hostnet"
-            "proxynet"
-          ];
-          ports = [
-            "80:80"
-            "443:443"
-            "443:443/udp"
-          ];
-          capabilities = {
-            NET_ADMIN = true;
-          };
-          restart = "unless-stopped";
-          volumes = [
-            "${config.age.secrets.monolith_caddy_caddyfile.path}:/etc/caddy/Caddyfile"
-            "/data/docker/caddy/site:/srv"
-            "/data/docker/caddy/data:/data"
-            "/data/docker/caddy/config:/config"
-          ];
-        };
-        "mariadb".service = {
-          container_name = "mariadb";
-          image = "docker.io/mariadb:latest";
-          env_file = [config.age.secrets.monolith_docker_env_mariadb.path];
-          networks = ["datanet"];
-          restart = "unless-stopped";
-          volumes = [
-            "/data/docker/mariadb/mysql:/var/lib/mysql"
-          ];
-        };
         "romm".service = {
           container_name = "romm";
           image = "docker.io/rommapp/romm:latest";
@@ -70,6 +100,18 @@
             "proxynet"
             "datanet"
           ];
+          healthcheck = {
+            test = [
+              "CMD"
+              "curl"
+              "--fail"
+              "http://127.0.0.1:8080"
+            ];
+            start_period = "60s";
+            interval = "60s";
+            timeout = "5s";
+            retries = 3;
+          };
           restart = "unless-stopped";
           volumes = [
             "/data/docker/romm/resources:/romm/resources"
@@ -77,6 +119,67 @@
             "/data/docker/romm/assets:/romm/assets"
             "/data/docker/romm/redis:/redis-data"
             "/nas/roms:/romm/library"
+          ];
+        };
+        "kavita".service = {
+          container_name = "kavita";
+          image = "lscr.io/linuxserver/kavita:latest";
+          environment = {
+            PUID = "4000";
+            PGID = "4000";
+            TZ = "America/Phoenix";
+          };
+          networks = ["proxynet"];
+          healthcheck = {
+            test = [
+              "CMD"
+              "curl"
+              "--fail"
+              "http://127.0.0.1:5000"
+            ];
+            start_period = "60s";
+            interval = "60s";
+            timeout = "5s";
+            retries = 3;
+          };
+          restart = "unless-stopped";
+          volumes = [
+            "/data/docker/kavita/config:/config"
+            "/nas/media/Books:/mnt/books"
+          ];
+        };
+        "ersatztv".service = {
+          container_name = "ersatztv";
+          image = "docker.io/jasongdove/ersatztv:latest-vaapi";
+          ports = ["8409:8409"];
+          environment = {
+            TZ = "America/Phoenix";
+          };
+          networks = [
+            "hostnet"
+            "proxynet"
+          ];
+          healthcheck = {
+            test = [
+              "CMD"
+              "curl"
+              "--fail"
+              "http://127.0.0.1:8409"
+            ];
+            start_period = "60s";
+            interval = "60s";
+            timeout = "5s";
+            retries = 3;
+          };
+          restart = "unless-stopped";
+          tmpfs = ["/transcode,size=4g"];
+          volumes = [
+            "/data/docker/ersatztv/config:/config"
+            "/nas/media:/mnt/plex:ro"
+          ];
+          devices = [
+            "/dev/dri/card0:/dev/dri/card0"
+            "/dev/dri/renderD128:/dev/dri/renderD128"
           ];
         };
       };
