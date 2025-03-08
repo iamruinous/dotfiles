054c91d8c413ce32b47584b246a8f6230783243d
Jade Meskill <jade.meskill@gmail.com>
2025-03-08 10:01:03 -0700

    explicitly enable wireless on kidmacnix

--- nix-config/hosts/kidmacnix/hardware-configuration.nix
+++ nix-config/hosts/kidmacnix/hardware-configuration.nix
@@ -31,7 +31,8 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0f0.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp4s0f0.useDHCP = lib.mkDefault true;
+  networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
