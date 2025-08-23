b20191cf3b968277222b0f77169b867bdf78934e
Jade Meskill <jade.meskill@gmail.com>
2025-08-23 11:04:40 -0700

    add windows bootstrap scripts

---
+++ win-config/install/admin-install.ps1
@@ -0,0 +1,26 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`""
+    exit
+}
+
+function InstallFonts
+{
+    $error.Clear()
+
+    Get-InstalledPSResource -Name NerdFonts -Scope AllUsers | Out-Null
+    if (-not($?))
+    {
+        Set-PSResourceRepository -Name "PSGallery" -Priority 25 -Trusted -PassThru
+        Install-PSResource -Name Fonts -Scope AllUsers -Reinstall -Version '1.1.18'
+        Install-PSResource -Name NerdFonts -Scope AllUsers -Reinstall
+    }
+    Import-Module -Name NerdFonts
+    Get-NerdFont -Name 'FiraCode'
+    if (-not($?))
+    {  
+        Install-NerdFont -Name 'FiraCode' -Scope AllUsers
+    }
+}
+
+InstallFonts
---
+++ win-config/install/install.ps1
@@ -0,0 +1,106 @@
+param ($ComputerName = $(throw "ComputerName parameter is required."))
+
+$currentDirectory = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\') 
+if ($currentDirectory -eq $PSHOME.TrimEnd('\')) 
+{     
+    $currentDirectory = $PSScriptRoot 
+}
+
+function InstallPackages
+{
+    $error.Clear()
+
+    winget install `
+        --exact `
+        7zip.7zip `
+        Agilebits.1Password `
+        Agilebits.1Password.CLI `
+        eza-community.eza `
+        GitHub.GitHubDesktop `
+        Google.Chrome.EXE `
+        JesseDuffield.lazygit `
+        LGUG2Z.komorebi `
+        LGUG2Z.whkd `
+        Microsoft.Git `
+        Microsoft.PowerShell `
+        Microsoft.PowerToys `
+        Microsoft.VisualStudioCode `
+        Microsoft.WSL `
+        Microsoft.WindowsTerminal `
+        Neovim.Neovim `
+        OpenJS.NodeJS `
+        Plex.Plex `
+        PrivateInternetAccess.PrivateInternetAccess `
+        Starship.Starship `
+        Tailscale.Tailscale `
+        Valve.Steam
+
+    $MINGW_ID="MartinStorsjo.LLVM-MinGW.UCRT"
+    winget list -q $MINGW_ID | Out-Null
+    if (-not($?))
+    {  
+        winget install `
+            --exact `
+            --no-upgrade `
+            $MINGW_ID
+    }
+}
+
+function InstallFiles
+{
+    $error.Clear()
+
+    ## Nvim
+    $NVIM_CONFIG_DIR="$env:LOCALAPPDATA\nvim"
+
+    if (-not (Test-Path -Path "$NVIM_CONFIG_DIR"))
+    {
+        git clone --depth 1 https://github.com/AstroNvim/template "$NVIM_CONFIG_DIR"
+        Remove-Item $NVIM_CONFIG_DIR\.git -Recurse -Force
+    }
+
+    $XDG_CONFIG_DIR="$env:USERPROFILE\.config"
+    $DOTFILES_DIR="$env:USERPROFILE\Projects\github\iamruinous\dotfiles\win-config"
+
+
+    ## Komorebi
+    $KOMOREBI_CONFIG_DIR="$XDG_CONFIG_DIR\komorebi"
+
+    if (-not (Test-Path -Path "$KOMOREBI_CONFIG_DIR"))
+    {
+        mkdir -Force "$KOMOREBI_CONFIG_DIR"
+    }
+    Copy-Item "$DOTFILES_DIR\files\configs\komorebi\*.json" "$KOMOREBI_CONFIG_DIR"
+    Copy-Item "$DOTFILES_DIR\files\configs\whkdrc" "$XDG_CONFIG_DIR"
+    komorebic enable-autostart --config "$KOMOREBI_CONFIG_DIR\komorebi.json" --whkd
+
+    ## Starship
+    $STARSHIP_PROFILE="Invoke-Expression (&starship init powershell)"
+
+    if (-not (Test-Path -Path "$PROFILE"))
+    { 
+        New-Item -Path "$PROFILE" -ItemType File
+    }
+
+    if ( (Get-Content -Path "$PROFILE") -notcontains "$STARSHIP_PROFILE")
+    {
+        Add-Content -Path "$PROFILE" -Value "$STARSHIP_PROFILE"
+    }
+}
+
+function InstallAliases
+{
+    $error.Clear()
+
+    ## eza
+    $EZA_PROFILE='Set-Alias -Name ll -Value "eza --git --icons --long --header"'
+
+    if ( (Get-Content -Path "$PROFILE") -notcontains "$EZA_PROFILE")
+    {
+        Add-Content -Path "$PROFILE" -Value "$EZA_PROFILE"
+    }
+}
+
+InstallPackages $computerName
+InstallFiles $computerName
+InstallAliases $computerName
