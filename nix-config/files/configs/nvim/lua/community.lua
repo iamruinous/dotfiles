28605c4fa7f6d8c101ec0c0110de25e0c8480939
Jade Meskill <jade.meskill@gmail.com>
2025-09-09 21:25:57 -0400

    update astronvim to latest, remove avante

--- nix-config/files/configs/nvim/lua/community.lua
+++ nix-config/files/configs/nvim/lua/community.lua
@@ -10,5 +10,5 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  { import = "astrocommunity.completion.avante-nvim" },
+  -- { import = "astrocommunity.completion.avante-nvim" },
 }
