6d2ed137f274b94ddea197674ad86f138a078b4d
Jade Meskill <jade.meskill@gmail.com>
2025-09-09 18:32:15 -0700

    don't install mason tools because nix

--- nix-config/files/configs/nvim/lua/user.lua
+++ nix-config/files/configs/nvim/lua/user.lua
@@ -9,3 +9,7 @@ require("mason-null-ls").setup {
   ensure_installed = {},
   automatic_installation = false,
 }
+require('mason-tool-installer').setup {
+  ensure_installed = {},
+  run_on_start = false,
+}
